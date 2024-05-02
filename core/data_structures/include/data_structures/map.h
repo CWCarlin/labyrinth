57d0c5a00ea5b8df1c506000d6036c7d172be1b0
cwcarlin <chandlercarlin01@gmail.com>
2024-05-02 14:13:28 -0500

    cleaned up before refactor

--- core/data_structures/include/data_structures/map.h
+++ core/data_structures/include/data_structures/map.h
@@ -4,19 +4,20 @@
 #include "data_structures/hash.h"
 
 typedef struct lbr_map_t {
-	LbrAllocCallback alloc_callback;
-	u8* data;
-	usize capacity;
-	usize key_size;
-	usize value_size;
-	usize length;
+  LbrAllocCallback alloc_callback;
+  u8* data;
+  usize capacity;
+  usize key_size;
+  usize value_size;
+  usize length;
 
-	PFN_lbrHashFunc hash;
-	PFN_lbrEqualityFunc equals;
+  PFN_lbrHashFunc hash;
+  PFN_lbrEqualityFunc equals;
 } LbrMap;
 
-void lbrCreateMap(LbrMap* p_map, usize capacity, usize key_size, usize value_size,
-	LbrAllocCallback alloc_callback, PFN_lbrHashFunc hash, PFN_lbrEqualityFunc equals);
+void lbrCreateMap(LbrMap* p_map, usize capacity, usize key_size,
+                  usize value_size, LbrAllocCallback alloc_callback,
+                  PFN_lbrHashFunc hash, PFN_lbrEqualityFunc equals);
 void lbrDestroyMap(LbrMap* p_map);
 void lbrMapInsert(LbrMap* p_map, void* p_key, void* p_value);
 void* lbrMapGetValue(LbrMap* p_map, void* p_key);
--- main.c
+++ main.c
@@ -1,42 +1,38 @@
+#include <stdio.h>
+#include <vulkan/vulkan.h>
+
+#include "allocators/alloc_info.h"
+#include "allocators/linear_alloc.h"
 #include "data_structures/hash.h"
 #include "data_structures/map.h"
-#include "allocators/linear_alloc.h"
-#include "allocators/alloc_info.h"
-
-#include <stdio.h>
 
 typedef struct {
-	int one, two;
+  int one, two;
 } test;
 
-usize inthash(int* i) {
-	return *i;
-}
+usize inthash(int* i) { return *i; }
 
-u8 intequals(int* a, int* b) {
-	return *a == *b;
-}
+u8 intequals(int* a, int* b) { return *a == *b; }
 
 int main() {
-	LbrLinearAllocator alloc;
-	lbrCreateLinearAllocator(&alloc, 1024);
-	LbrAllocCallback callback = lbrLinearAllocatorGetAllocCallback(&alloc);
+  LbrLinearAllocator alloc;
+  lbrCreateLinearAllocator(&alloc, 1024);
+  LbrAllocCallback callback = lbrLinearAllocatorGetAllocCallback(&alloc);
 
-	LbrMap map;
-	lbrCreateMap(&map, 16, sizeof(int), sizeof(test),
-		callback, (PFN_lbrHashFunc)&inthash, (PFN_lbrEqualityFunc)&intequals);
+  LbrMap map;
+  lbrCreateMap(&map, 16, sizeof(int), sizeof(test), callback,
+               (PFN_lbrHashFunc)&inthash, (PFN_lbrEqualityFunc)&intequals);
 
+  int i = 1;
+  test t = {1, 1};
 
-	int i = 1;
-	test t = {1, 1};
+  lbrMapInsert(&map, &i, &t);
 
-	lbrMapInsert(&map, &i, &t);
+  printf("%llu\n", map.length);
 
-	printf("%llu\n", map.length);
+  i = 1;
+  test t_2 = *(test*)lbrMapGetValue(&map, &i);
+  printf("%d %d\n", t_2, t_2.two);
 
-	i = 2;
-	test t_2 = *(test*)lbrMapGetValue(&map, &i);
-	printf("%d %d\n", t_2.one, t_2.two);
-
-	return 0;
+  return 0;
 }
