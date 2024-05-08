c144c1599853ff96638896c32a44ce92fb637e7b
cwcarlin <chandlercarlin01@gmail.com>
2024-05-08 14:39:26 -0500

    fibers are still broken. Reworking.

--- main.c
+++ main.c
@@ -1,36 +1,8 @@
 #include <stdio.h>
 
-#include "fabric/fabric.h"
 #include "fabric/fiber.h"
-#include "fabric/lock.h"
-#include "utils/types.h"
-
-LbrSpinLock lock = {100};
-
-void test() {
-  lbrSpinLockDecrement(&lock);
-
-  printf("Hello Fiber\n");
-
-  lbrFabricReturn();
-}
 
 int main() {
-  lbrInitializeFabric();
-
-  LbrTask tasks[100];
-
-  for (int i = 0; i < 100; i++) {
-    tasks[i].p_entry_point = (PFN_lbrEntryFunction)test;
-  }
-
-  lbrFabricQueueTasks(tasks, 100);
-
-  while (lock.acquired) {
-    printf("%d\n", lock.acquired);
-  }
-
-  printf("Done!\n");
 
   return 0;
 }
